7842913546f16658a9c64a85c6ac0f3f4351dc7f
Ragavi14 <ragavi2002l@gmail.com>
2026-02-13 14:37:43 +0530

    changes

---
+++ src/lib/langMap.ts
@@ -0,0 +1,13 @@
+export const LANGUAGE_MAP: Record<string, string> = {
+    EN: "en",
+    FR: "fr",
+    DE: "de",
+    NL: "nl",
+    PT: "pt",
+    IT: "it",
+    ES: "es",
+    PL: "pl",
+    SE: "sv",
+    FI: "fi",
+  };
+  
